632f01dfeae2e07965051113c41ccc5931137fab
Tracy Li <tibcotracy@gmail.com>
2016-09-22 14:36:58 -0500

    Add more condition to convert a data type. it's better return empty array or map rather than nil.

--- core/data/coercion.go
+++ core/data/coercion.go
@@ -146,6 +146,10 @@ func CoerceToObject(val interface{}) (map[string]interface{}, error) {
 	switch t := val.(type) {
 	case map[string]interface{}:
 		return t, nil
+	case nil:
+		return make(map[string]interface{}), nil
+	case string:
+		return t, nil
 	default:
 		return nil, fmt.Errorf("Unable to coerce %#v to map[string]interface{}", val)
 	}
@@ -163,6 +167,17 @@ func CoerceToArray(val interface{}) ([]interface{}, error) {
 			a = append(a, v)
 		}
 		return a, nil
+	case nil:
+		return make([]interface{}, 0), nil
+	case string:
+		if len(t) <= 0 {
+			return make([]interface{}, 0), nil
+
+		} else {
+			str := make([]interface{}, 1)
+			str[0] = val
+			return str, nil
+		}
 	default:
 		return nil, fmt.Errorf("Unable to coerce %#v to []interface{}", val)
 	}
@@ -215,6 +230,15 @@ func CoerceToParams(val interface{}) (map[string]string, error) {
 			m[mKey] = mVal
 		}
 		return m, nil
+	case string:
+		if len(t) <= 0 {
+			return make(map[string]string), nil
+
+		} else {
+			return nil, fmt.Errorf("Unable to coerce %#v to map[string]string", val)
+		}
+	case nil:
+		return make(map[string]string), nil
 	default:
 		return nil, fmt.Errorf("Unable to coerce %#v to map[string]string", val)
 	}
