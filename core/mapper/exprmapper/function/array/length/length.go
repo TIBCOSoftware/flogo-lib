ee5b327ae924fa305ed525cc8dc39ee08dba9576
retgits <lstigter@gmail.com>
2018-05-25 15:06:59 -0700

    Added error return and testcase

--- core/mapper/exprmapper/function/array/length/length.go
+++ core/mapper/exprmapper/function/array/length/length.go
@@ -1,6 +1,8 @@
 package length
 
 import (
+	"fmt"
+
 	"github.com/TIBCOSoftware/flogo-lib/core/data"
 	"github.com/TIBCOSoftware/flogo-lib/core/mapper/exprmapper/expression/function"
 	"github.com/TIBCOSoftware/flogo-lib/logger"
@@ -23,11 +25,11 @@ func (s *Length) GetCategory() string {
 	return "array"
 }
 
-func (s *Length) Eval(arr interface{}) int {
+func (s *Length) Eval(arr interface{}) (int, error) {
 	log.Debugf("Return the length of array \"%v\"", arr)
 	myArr, err := data.CoerceToArray(arr)
 	if err != nil {
-		log.Errorf(err.Error())
+		return 0, fmt.Errorf(err.Error())
 	}
-	return len(myArr)
+	return len(myArr), nil
 }
--- core/mapper/exprmapper/function/array/length/length_test.go
+++ core/mapper/exprmapper/function/array/length/length_test.go
@@ -15,13 +15,18 @@ func TestLength(t *testing.T) {
 
 	// Should produce "3"
 	arr := []string{"1", "2", "3"}
-	sub := s.Eval(arr)
+	sub, _ := s.Eval(arr)
 	fmt.Printf("Result [%v] should be equal to: 3\n", sub)
 	assert.Equal(t, 3, sub)
 
 	// Should produce "5"
 	arr2 := []int{1, 2, 3, 4, 5}
-	sub = s.Eval(arr2)
+	sub, _ = s.Eval(arr2)
 	fmt.Printf("Result [%v] should be equal to: 5\n", sub)
 	assert.Equal(t, 5, sub)
+
+	// Should produce an error
+	arr3 := "Hello!"
+	_, err := s.Eval(arr3)
+	fmt.Printf("Result [%v] should contain: unable to coerce\n", err)
 }
