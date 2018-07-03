05acc9d1623085a4ab554fd457ff524ff8b510ad
Andy Hampshire <ahampshi@tibco.com>
2018-07-03 12:13:42 +0100

    Add simple string conversion function

---
+++ core/mapper/exprmapper/function/string/tostring/tostring.go
@@ -0,0 +1,30 @@
+package tostring
+
+import (
+	"fmt"
+	"github.com/TIBCOSoftware/flogo-lib/core/mapper/exprmapper/expression/function"
+	"github.com/TIBCOSoftware/flogo-lib/logger"
+)
+
+var log = logger.GetLogger("equals-function")
+
+type ToString struct {
+}
+
+func init() {
+	function.Registry(&ToString{})
+}
+
+func (s *ToString) GetName() string {
+	return "toString"
+}
+
+func (s *ToString) GetCategory() string {
+	return "string"
+}
+func (s *ToString) Eval(inp interface{}) string {
+
+	result := fmt.Sprintf("%v", inp)
+	log.Debugf(`Converts value to a simple string" `, result)
+	return result
+}
